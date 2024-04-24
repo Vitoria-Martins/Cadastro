0a34d8a8b509f348c605bbc85be48950799a20fa
vitoria299 <vitorianogueira299@gmail.com>
2024-04-24 11:15:46 -0300

    html css ready

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Hello word!!");
+function abre_login() {
+  window.open("login.html", "_blank");
+}
+
+function abre_cadastro() {
+  window.open("cadastrar.html", "_blank");
+}
+
+function enviarToken() {
+  alert("Um Token foi enviado para seu email");
+}
+
+function cadastrar() {
+  alert("Cadastro Realizado com sucesso!");
+}
